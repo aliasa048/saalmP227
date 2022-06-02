9e3aa0cd7515b9e141d9402c3bf1115e9f839aa6
Eli <aliasa@code.edu.az>
2022-06-02 12:02:36 +0400

    HomeWork 02.06.2022

---
+++ hometask 02.06.2022/hometask 02.06.2022/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace hometask_02._06._2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user1 = new User();
+
+            user1.SetPassword("Salam345");
+
+            Console.WriteLine(user1.GetPassword());
+           
+        }
+    }
+}
---
+++ hometask 02.06.2022/hometask 02.06.2022/User.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace hometask_02._06._2022
+{
+     internal class User
+     {
+        public string UserName;
+        private string Password;
+
+        public void SetPassword(string password)
+        {
+            if (string.IsNullOrWhiteSpace(password)== false && password.Length > 7 && password.Length < 25 && HasUpper(password) && HasDigit(password)) 
+              this.Password = password;
+        }
+
+        public string GetPassword()
+        {
+            return this.Password;
+        }
+
+        public bool HasUpper(string str)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (Char.IsUpper(str[i]))
+                    return true;
+
+            }
+            return false;
+        }
+
+        public bool HasDigit(string str)
+        {
+            for (int i =0; i< str.Length; i++)
+            {
+                if (Char.IsDigit(str[i]))
+                    return true;
+            }
+            return false;
+
+        }
+     }
+}
